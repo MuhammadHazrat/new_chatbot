e4c229dc2a11121df264b624da489000b55ccbf0
Muhammad Hazrat <221441166@formanite.fccollege.edu.pk>
2022-12-18 09:45:56 +0500

    changes

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-port = int(os.environ.get('PORT', 5000))
+# port = int(os.environ.get('PORT', 5000))
 
 
 @app.route('/', methods=['POST', 'GET'])
@@ -33,4 +33,4 @@ def hello():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=port)
+    app.run()
